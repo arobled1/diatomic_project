d984fbe756fd4a1a9c8671c52309f07399f452fe
Alan Robledo <ar6138@nyu.edu>
2020-03-16 22:36:31 -0700

    added v and t matrix code

--- scripts/hamiltonian.py
+++ scripts/hamiltonian.py
@@ -3,22 +3,33 @@ from scipy import linalg as la
 import copy
 import matplotlib.pyplot as plt
 
-def vii_integrand():
-    return
+def vii_integrand(left, right, potential, index, num_points):
+    constant1 = 2/(right - left)
+    deltax = (float(right) - float(left)) / float(num_points - 1)
+    r = np.arange(left, right+deltax, deltax)
+    sinelist_v = (index*np.pi*(r - left) ) / (right - left)
+    sinelist_v = sinelist_v**2
+    return constant1 * potential * sinelist_v
 
-def tii_integrand():
+def tii_integrand(left, right, index, red_mass, num_points):
+    constant2 = ( (index * np.pi)**2 ) / (red_mass * (right - left))
+    deltax = (float(right) - float(left)) / float(num_points - 1)
+    r = np.arange(left, right+deltax, deltax)
+    sinelist_t = (index*np.pi*(r - left) ) / (right - left)
+    sinelist_t = sinelist_t**2
+    return constant2 * sinelist_t
 
-def comp_simpson(left, right, num_subint, func):
+def comp_simpson(left, right, num_subint, list):
     summation = 0
     dx = (float(right) - float(left)) / float(num_subint)
     i = 0
     while i < num_subint + 1:
         if i == 0 or i == num_subint:
-            summation = summation + func(left + i * dx)
+            summation = summation + list[i]
         if i % 2 == 0 and i != 0 and i != num_subint:
-            summation = summation + 2 * func(left + i * dx)
+            summation = summation + 2 * list[i]
         if i % 2 == 1 and i != num_subint:
-            summation = summation + 4 * func(left + i * dx)
+            summation = summation + 4 * list[i]
         i = i + 1
     area = (dx / 3) * summation
     return area
@@ -36,6 +47,8 @@ def bubble_sort(eig_energies, eig_vectors):
 
 r = [float(x.split()[1]) for x in open('potential.out').readlines()]
 potent = [float(x.split()[2]) for x in open('potential.out').readlines()]
+potent = np.asarray(potent)
+subintervals = len(potent) - 1
 n = len(r)
 # Left boundary
 r1 = r[0]
@@ -47,28 +60,22 @@ mu = 0.5
 # Create potential matrix
 pe_matrix = np.zeros((n, n))
 for i in range(n):
-    pe_matrix[i,i] =
+    v_integrand = vii_integrand(r1, r2, potent, i, n)
+    pe_matrix[i,i] = comp_simpson(r1, r2, subintervals, v_integrand)
 
-# Create kinetic matrix
-ke_matrix = np.zeros((n, n))
-ke_matrix[0][0] = -2
-ke_matrix[1][0] = 1
-ke_matrix[n - 1][n - 1] = -2
-ke_matrix[n - 2][n - 1] = 1
-for i in range(1, n - 1):
-    ke_matrix[i - 1][i] = 1
-    ke_matrix[i][i] = -2
-    ke_matrix[i + 1][i] = 1
-ke_matrix = (-1/(2*mu)) * ke_matrix
-
-hamiltonian = ke_matrix + pe_matrix
-eig_val, eig_vec = la.eig(hamiltonian)
-sort_eigval, sort_eigvec = bubble_sort(eig_val, eig_vec)
-f = open('2d_eigenvalues.dat', 'w+')
-f.write("n       E_n\n")
+t_matrix = np.zeros((n, n))
 for i in range(n):
-    f.write("%s %s\n" % (i+1, sort_eigval[i]))
-f.close()
+    t_integrand = tii_integrand(r1, r2, i, mu, n)
+    t_matrix[i,i] = comp_simpson(r1, r2, subintervals, t_integrand)
+
+# hamiltonian = ke_matrix + pe_matrix
+# eig_val, eig_vec = la.eig(hamiltonian)
+# sort_eigval, sort_eigvec = bubble_sort(eig_val, eig_vec)
+# f = open('2d_eigenvalues.dat', 'w+')
+# f.write("n       E_n\n")
+# for i in range(n):
+#     f.write("%s %s\n" % (i+1, sort_eigval[i]))
+# f.close()
 
 # groundprob = np.array([sort_eigvec[i][0]*sort_eigvec[i][0] for i in range(n)])
 # first_prob = np.array([sort_eigvec[i][1]*sort_eigvec[i][1] for i in range(n)])
